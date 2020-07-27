19c88b6975543b7dabb34fe8e0b821379e6504e4
Marcin Juranek <m.juranek@PGG.PL>
2020-07-27 13:20:01 +0200

    Protect from null value in method  GetClientFullTextSearch

--- UserInterface/SClientFullTextSearch.cs
+++ UserInterface/SClientFullTextSearch.cs
@@ -13,19 +13,24 @@ namespace salonfr.UserInterface
         private static  ISelectClient selectClient;
         public static Client GetClientFullTextSearch(string searchText, out int clientId)
         {
-
+            
             //error with non unique value in result
             selectClient = new SelectClient();
             List<Client> result = new List<Client>();
             result = selectClient.GetClients(SGetAllRowsFromSpecificTable.ClientSelectAllRowsQuery());
-            int id = result.Select(x => new
+            var client = result.Select(x => new
             {
                 id = x.client_id,
                 text = x.client_name + " " + x.client_sname + " " + x.client_phone
-            }).Where(y => y.text.Contains(searchText)).FirstOrDefault().id;
+            }).Where(y => y.text.Contains(searchText));
+            if (client.Any() == false)
+            {
+                clientId = -1;
+                return new Client { client_id = 0, client_name = "brak", client_phone = "brak", client_sname = "brak", client_description = "brak" };
+            }
 
-            var selectedClient = selectClient.GetClients(SGetAllRowsFromSpecificTable.ClientSelectAllRowsQuery()).Where(x => x.client_id == id).First();
-            clientId = id;
+            var selectedClient = selectClient.GetClients(SGetAllRowsFromSpecificTable.ClientSelectAllRowsQuery()).Where(x => x.client_id == client.First().id).First();
+            clientId = client.First().id;
             return selectedClient;
         }
     }
