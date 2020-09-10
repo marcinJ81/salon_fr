fbd316ef55db3962e9d65bed7c841d234736b499
Marcin Juranek <m.juranek@PGG.PL>
2020-09-10 13:27:03 +0200

    change path directory file location in sln file

---
+++ MVCProject2/Controllers/ReservationController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCProject2.Models;
+using salonfr;
+using salonfr.QuerySelect;
+using salonfrSource.ModelDB;
+using salonfrSource.QuerySelect;
+
+namespace MVCProject2.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private ISelectTableObject<Client> selectClient;
+        private ISelectTableObject<Services> selectServices;
+        private ISelectReservation selectReservation;
+        private IGetVReservation getVReservation;
+        private ISelectTableObject<Employee> selectEmployee;
+
+        public ReservationController()
+        {
+            this.selectClient = new SelectClient(); 
+            this.selectServices = new SelectServices();
+            this.selectReservation = new SelectReservation();
+            this.selectEmployee = new SelectEmployee();
+            this.getVReservation = new CreateViewVreservation(selectClient, selectReservation, selectServices, selectEmployee);
+        }
+
+        public IActionResult Index()
+        {
+            var result = getVReservation.GetVReservations();
+            return View(result);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
